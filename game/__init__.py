9fda0d0461902def872746469d08b275a8264c5b
Gabriel Harriman <gabe@wittlebee.com>
2013-04-17 14:42:12 -0700

    added version info

---
+++ game/__init__.py
@@ -0,0 +1,10 @@
+__author__ = 'Gabriel Harriman'
+__copyright__ = 'Copyright 2013, Gabriel Harriman'
+__credits__ = ['Gabriel Harriman, mynameisgabe.com, @mynameisgabe']
+
+
+__license__ = 'BSD'
+__version__ = '2013.04.22'
+__maintainer__ = 'Gabriel Harriman'
+__email__ = 'lord-app@mynameisgabe.com'
+__status__ = 'Beta'
